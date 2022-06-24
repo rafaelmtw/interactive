a62d0d1ce2f555108b2f55067923a902ef1bcb76
rafaelmtw <u3574242@connect.hku.hk>
2022-06-24 17:10:24 +0800

    key mapping

--- components/People.js
+++ components/People.js
@@ -8,7 +8,7 @@ export default function People() {
       <p>We have {people.length} people!</p>
       <ul>
         {people.map((p) => (
-          <li>{p}</li>
+          <li key={Math.random()}>{p}</li>
         ))}
       </ul>
     </div>
